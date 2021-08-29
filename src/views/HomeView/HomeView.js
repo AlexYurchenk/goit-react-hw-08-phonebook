c669f1ed662e4106079e964b36d6ae18feb30e0e
AlexYurchenk <yurchenkoalex13@gmail.com>
2021-08-29 05:42:19 +0300

    gfd

--- src/views/HomeView/HomeView.js
+++ src/views/HomeView/HomeView.js
@@ -1,13 +1,13 @@
-import HomeIcon from '@material-ui/icons/Home';
-import s from './HomeView.module.css';
-import { Typography } from '@material-ui/core';
+import HomeIcon from "@material-ui/icons/Home";
+import s from "./HomeView.module.css";
+import { Typography } from "@material-ui/core";
 
 const HomeView = () => {
   return (
     <div className={s.wrapper}>
       <HomeIcon color="primary" className={s.img} />
       <Typography color="primary" variant="h3">
-        Page not found
+        This is home for my contacts
       </Typography>
     </div>
   );
